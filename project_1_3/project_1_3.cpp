91efd35e6d302d22eef19e8ad0617317f35be34c
Udit Subramanya <udit610@yahoo.com>
2020-09-24 21:30:08 +0000

    first attempt at convolution

--- project_1_3/project_1_3.cpp
+++ project_1_3/project_1_3.cpp
@@ -38,9 +38,50 @@ int main() {
   // TODO write your code here
   // =========== START =========
 
+  // Print x vector
+  std::cout << "x: {";
+  for (int i = 0; i < x.size(); i++) {
+    std::cout << x[i] << (i != (x.size()-1) ? ", " : "");
+  }
+  std::cout << "}" << std::endl;
 
+  // Print w vector
+  std::cout << "w: {";
+  for (int i = 0; i < w.size(); i++) {
+    std::cout << w[i] << (i != (w.size()-1) ? ", " : "");
+  }
+  std::cout << "}" << std::endl;
 
+  int buf = (w.size()-1)/2;
 
+  // 2D loop to convolude vector x
+  for (int i = 0; i < x.size(); i++) {
+    double sum = 0;
+    for (int j = 0; j < w.size(); j++) {
+      // standard handling
+      if(i - buf + j >= 0 && i - buf + j < x.size()) {
+        sum += x[i - buf + j] * w[j];
+      } 
+      // underflow handling
+      else if (!pack_with_zeros && i - buf + j < 0) {
+        sum += x[0] * w[j];
+      }
+      // over flow handling
+      else if (!pack_with_zeros && i - buf + j >= x.size()) {
+        sum += x[x.size() -1] * w[j];
+      }
+    }
+    // store in output vector
+    y.push_back(sum);
+  }
+
+  // Print y vector
+  std::cout << "y: {";
+  for (int i = 0; i < y.size(); i++) {
+    std::cout << y[i] << (i != (y.size()-1) ? ", " : "");
+  }
+  std::cout << "}" << std::endl;
+  
   // =========== END ===========
 
   return 0;
